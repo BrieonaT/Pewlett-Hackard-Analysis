18aab8275c88b4d9450f48a960f43b2f4067fbdb
Brieona <brieona.turner@gmail.com>
2021-07-22 15:15:58 -0400

    Updating Schema and Queries

--- schema.sql
+++ schema.sql
@@ -6,8 +6,10 @@ CREATE TABLE departments (
      UNIQUE (dept_name)
 );
 
+
+--Emp_no table
 CREATE TABLE employees (
-	     emp_no INT NOT NULL,
+     emp_no INT NOT NULL,
      birth_date DATE NOT NULL,
      first_name VARCHAR NOT NULL,
      last_name VARCHAR NOT NULL,
@@ -16,8 +18,10 @@ CREATE TABLE employees (
      PRIMARY KEY (emp_no)
 );
 
+
+--Dept Manager table
 CREATE TABLE dept_manager (
-    dept_no VARCHAR(4) NOT NULL,
+dept_no VARCHAR(4) NOT NULL,
     emp_no INT NOT NULL,
     from_date DATE NOT NULL,
     to_date DATE NOT NULL,
@@ -26,6 +30,7 @@ FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
     PRIMARY KEY (emp_no, dept_no)
 );
 
+--Salaries Table
 CREATE TABLE salaries (
   emp_no INT NOT NULL,
   salary INT NOT NULL,
@@ -35,91 +40,23 @@ CREATE TABLE salaries (
   PRIMARY KEY (emp_no)
 );
 
-CREATE TABLE titles(
-  emp_no INT NOT NULL,
-  title INT NOT NULL,
-  from_date DATE NOT NULL,
-  to_date DATE NOT NULL,
-  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-  PRIMARY KEY (emp_no)
-);
 
-CREATE TABLE dept_emp(
-  dept_no VARCHAR(4) NOT NULL,
-  emp_no INT NOT NULL,
-  from_date DATE NOT NULL,
-  to_date DATE NOT NULL,
-  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-  FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
-    PRIMARY KEY (emp_no, dept_no)
-);
-
-SELECT * FROM departments;
-
-
-
-
-
-
-
-
-
-
--- Creating tables for PH-EmployeeDB
-CREATE TABLE departments (
-     dept_no VARCHAR(4) NOT NULL,
-     dept_name VARCHAR(40) NOT NULL,
-     PRIMARY KEY (dept_no),
-     UNIQUE (dept_name)
+-- Dept emp table
+CREATE TABLE dept_emp (
+ emp_no INT   NOT NULL,
+ dept_no VARCHAR NOT NULL,
+ from_date date NOT NULL,
+ to_date date NOT NULL,
+ FOREIGN KEY (emp_no) REFERENCES Employees(emp_no),
+ FOREIGN KEY (dept_no) REFERENCES Departments(dept_no)
 );
 
 
-CREATE TABLE employees (
-	     emp_no INT NOT NULL,
-     birth_date DATE NOT NULL,
-     first_name VARCHAR NOT NULL,
-     last_name VARCHAR NOT NULL,
-     gender VARCHAR NOT NULL,
-     hire_date DATE NOT NULL,
-     PRIMARY KEY (emp_no)
-);
-
-CREATE TABLE dept_manager (
-    dept_no VARCHAR(4) NOT NULL,
-    emp_no INT NOT NULL,
-    from_date DATE NOT NULL,
-    to_date DATE NOT NULL,
-FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
-    PRIMARY KEY (emp_no, dept_no)
-);
-
-CREATE TABLE salaries (
-  emp_no INT NOT NULL,
-  salary INT NOT NULL,
-  from_date DATE NOT NULL,
-  to_date DATE NOT NULL,
-  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-  PRIMARY KEY (emp_no)
-);
-
+--Titles Table
 CREATE TABLE titles (
-    emp_no      INT             NOT NULL,
-    title       VARCHAR(50)     NOT NULL,
-    from_date   DATE            NOT NULL,
-    to_date     DATE,
-    FOREIGN KEY (emp_no) REFERENCES employees (emp_no) ON DELETE CASCADE,
-    PRIMARY KEY (emp_no,title, from_date)
-); 
-
-CREATE TABLE salaries (
-    emp_no      INT             NOT NULL,
-    salary      INT             NOT NULL,
-    from_date   DATE            NOT NULL,
-    to_date     DATE            NOT NULL,
-    FOREIGN KEY (emp_no) REFERENCES employees (emp_no) ON DELETE CASCADE,
-    PRIMARY KEY (emp_no, from_date)
-); 
-SELECT * FROM departments;
-
-DROP TABLE employees CASCADE;
+ emp_no int   NOT NULL,
+ title VARCHAR   NOT NULL,
+ from_date date   NOT NULL,
+ to_date date   NOT NULL,
+ FOREIGN KEY (emp_no) REFERENCES Employees(emp_no)
+);
